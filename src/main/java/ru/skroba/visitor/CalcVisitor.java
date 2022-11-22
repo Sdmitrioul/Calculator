780f93984f328a9613cf56a436bec496130be899
”Dmitri <dmitri.zd1958@gmail.com>
2022-11-22 17:20:11 +0100

    Task: add Calc visitor

--- src/main/java/ru/skroba/visitor/CalcVisitor.java
+++ src/main/java/ru/skroba/visitor/CalcVisitor.java
@@ -1,22 +1,51 @@
 package ru.skroba.visitor;
 
+import ru.skroba.exception.VisitorException;
 import ru.skroba.token.BraceToken;
 import ru.skroba.token.NumberToken;
 import ru.skroba.token.OperationToken;
+import ru.skroba.token.Token;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CalcVisitor implements TokenVisitor {
+    private Deque<Integer> queue;
+    
+    public synchronized int calculate(final List<Token> tokens) throws VisitorException {
+        queue = new LinkedList<>();
+        
+        tokens.forEach(it -> it.accept(this));
+        
+        final int result = queue.isEmpty() ? 0 : queue.poll();
+        
+        if (!queue.isEmpty()) {
+            throw new VisitorException("Queue is not empty: lost: " + queue.stream()
+                    .map(Object::toString)
+                    .collect(Collectors.joining(" ")));
+        }
+        
+        return result;
+    }
+    
     @Override
     public void visit(final NumberToken token) {
-        //TODO
+        queue.add(token.value());
     }
     
     @Override
     public void visit(final BraceToken token) {
-        //TODO
+        throw new UnsupportedOperationException("This operation isn't allowed! Tokens list is not well formed!");
     }
     
     @Override
     public void visit(final OperationToken token) {
-        //TODO
+        if (queue.size() < 2) {
+            throw new UnsupportedOperationException("This operation isn't allowed! Tokens list is not well formed!");
+        }
+        
+        queue.push(token.eval(queue.pop(), queue.pop()));
     }
 }
